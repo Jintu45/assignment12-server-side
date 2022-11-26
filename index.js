fbd7a2dc78bc85dc6edd9f714092e6127201e729
Zintu paul <jintupaul45@gmail.com>
2022-11-26 10:23:30 +0600

    add categorycollection

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
-
 require('dotenv').config()
 
 app.use(cors())
@@ -12,14 +12,40 @@ app.use(express.json())
 const category = require('./category.json')
 const product = require('./product.json')
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t6zznhm.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const categoriesCollection = client.db('assignment12').collection('category')
+        const productsCollection = client.db('assignment12').collection('products')
+
+        app.get('/categories', async(req, res) => {
+            const query = {}
+            const cursor = categoriesCollection.find(query)
+            const category = await cursor.toArray()
+            res.send(category)
+        })
+
+        app.get('/categories/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {category_id:id}
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+        })
+    }
+    finally{}
+}
+run().catch(error => console.error(error))
+
 app.get('/', (req, res) => {
     res.send('server is running')
 })
-
-app.get('/category', (req, res) => {
-    res.send(category)
-})
-
 
 app.listen(port, () => {
     console.log(`assignment11 server is running ${port}`)
