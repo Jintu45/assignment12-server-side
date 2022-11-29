44e74759949e286eaee1e8c7bcc21083cfbbaead
Zintu paul <jintupaul45@gmail.com>
2022-11-30 02:18:07 +0600

    added product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -44,12 +44,67 @@ async function run(){
             res.send(result)
         })
 
+        app.post('/addProduct', async(req,res)=>{
+            const product = req.body
+            console.log(product);
+            const result = await productsCollection.insertOne(product)
+            res.send(result)
+        })
+
         //get bookings buyer bookin data from mongodb
-        app.get('/bookings', async (req,res)=>{
+        // app.get('/bookings', async (req,res)=>{
+        //     const email = req.query.email;
+        //     const query = {email: email}
+        //     const cursor = await buyerBookingsCollection.find(query).toArray()
+        //     res.send(cursor)
+        // })
+    
+        app.get('/bookings', async(req, res) => {
             const email = req.query.email;
             const query = {email: email}
-            const cursor = await buyerBookingsCollection.find(query).toArray()
-            res.send(cursor)
+            const bookings = await buyerBookingsCollection.find(query).toArray()
+            res.send(bookings)
+        })
+
+        app.post('/bookings', async(req, res) => {
+            const booking = req.body;
+            console.log(booking)
+            const query = {}
+            const alreadyBooked = await buyerBookingsCollection.find(query).toArray()
+            console.log(alreadyBooked)
+            if(alreadyBooked.length){
+                const message = `you have a booking ${booking.appointmentDate}`
+                return res.send({acknowledge: false, message})
+            }
+            const result = await buyerBookingsCollection.insertOne(booking)
+            res.send(result)
+        })
+
+        // app.delete('/bookings/:id', async(req, res)=>{
+        //     const id = req.params.id;
+        //     const query = {_id: ObjectId(id)}
+        //     const result = await buyerBookingsCollection.deleteOne(query)
+        //     res.send(result)
+        // })
+
+        app.get('/reviews', async(req, res) => {
+            console.log(req.query.email)
+
+            let query = {}
+            if(req.query.email){
+                query={
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review)
+        })
+
+        app.post('/reviews', async(req, res) => {
+            const order = req.body;
+            const result = await reviewCollection.insertOne(order);
+            res.send(result)
         })
 
         app.delete('/reviews/:id', async(req, res)=>{
